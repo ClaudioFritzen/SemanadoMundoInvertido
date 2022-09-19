5e715d6a01a71b7a8b2480555616a0f28f1093bd
ClaudioFritzen <sdfritzen96@gmail.com>
2022-09-19 19:04:36 +0100

    intro ao firebase e importando por tag html

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB_qKmT3oyFQEocam_vyc180H-umGfNyUo",
+    authDomain: "semana-do-mundo-invertido.firebaseapp.com",
+    projectId: "semana-do-mundo-invertido",
+    storageBucket: "semana-do-mundo-invertido.appspot.com",
+    messagingSenderId: "212378302900",
+    appId: "1:212378302900:web:a4d11ed0d8b4661ac45ae1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
