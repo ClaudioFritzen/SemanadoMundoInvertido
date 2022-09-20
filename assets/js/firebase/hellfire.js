a82a9addecac9c4ca29d123a724e7c666ed5fa95
ClaudioFritzen <sdfritzen96@gmail.com>
2022-09-20 18:05:33 +0100

    estrutura basica

--- assets/js/firebase/hellfire.js
+++ assets/js/firebase/hellfire.js
@@ -1,7 +1,12 @@
 import app from "./app.js"
 
-import { } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-firebase.js'
+import { getFirestore, collection, addDoc, getDocs} from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js'
 
 export function subscribeToHellFireClub(subscription){
-    console.log('Subscription:', subscription)
-}
+    const db = getFirestore(app)
+    const hellfireCollection =  collection(db, "hellfire")
+    // função assincrona
+    addDoc(hellfireCollection, subscription)
+
+    // como esperando uma promessa 
+};
